46912830723c381245e89e585c56b5fe932583be
Rizka Amelia <ameliarizka18@gmail.com>
2023-06-16 00:27:22 +0700

    pert_11

---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'dart:convert';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'HTTP Request',
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List dataList = [];
+  Future<void> fetchData() async {
+    var url = Uri.parse('https://jsonplaceholder.typicode.com/albums');
+    var response = await http.get(url);
+    if (response.statusCode == 200) {
+      dataList = jsonDecode(response.body);
+    } else {
+      print('Request failed with status: ${response.statusCode}.');
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('HTTP Request'),
+      ),
+      body: ListView.builder(
+        itemCount: dataList.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(dataList[index]['title']),
+          );
+        },
+      ),
+    );
+  }
+}
